9618b3910b45b09e5c2bf1074cf4850c3a650c92
turnback <274114318@qq.com>
2023-02-12 16:55:33 +0800

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { t, availableLocales, locale } = useI18n()
+
+</script>
+
+<template>
+  <nav text-xl mt-6>
+    <button class="icon-btn mx-2 !outline-none" :title="t('button.toggle_dark')" @click="toggleDark()">
+      <div i="carbon-sun dark:carbon-moon" />
+    </button> 
+  </nav>
+</template>
---
+++ src/layouts/home.vue
@@ -0,0 +1,6 @@
+<template>
+  <main px-4 py-10 text-center text-gray-700 dark:text-gray-100>
+    <RouterView />
+    <Footer />
+  </main>
+</template>
